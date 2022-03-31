5884fc33931ef9d3f29e5ca68d73b44a2b2d6601
Indrajit Roy <70997867+indrajit-roy@users.noreply.github.com>
2022-03-31 16:09:17 +0530

    added endpoint to modify apple-app-site-association

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 
 const port = 3000
@@ -9,10 +14,28 @@ app.get('/', (req, res) => {
   res.send(Buffer.from('<a href="https://www.flexofficial.in">Trigger app!!</a>'));
 })
 
-app.get('/applinkstest/.well-known/assetlinks.json', (req, res) => {
+
+app.get('/applinkstest/.well-known/assetlinks', (req, res) => {
    res.header("Content-Type",'application/json');
    res.sendFile(path.join(__dirname, '/public/.well-known/assetlinks.json'));
 })
+
+app.post('/update-apple-app-site-association', function(request, respond) {
+  let body = '';
+  const root = __dirname;
+  
+  const filePath = path.join(__dirname,'/public/.well-known/apple-app-site-association');
+  request.on('data', function(data) {
+      body += data;
+  });
+
+  request.on('end', function (){
+    console.log(`on end body : ${body}`);
+    fs.writeFile(filePath, body, function() {
+      respond.end();
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
